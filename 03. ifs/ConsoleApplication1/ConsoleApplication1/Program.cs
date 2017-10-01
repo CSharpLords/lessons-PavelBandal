92fe555255704d3ba03b384ae1dd106480706ad9
PavelBandal <pavel.bandal@ya.ru>
2017-10-01 16:57:12 +0300

    Число игр

---
+++ 03. ifs/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввелите число игр");
+            Console.ReadLine();
+            int a = int.Parse(Console.ReadLine());
+        }
+    }
+}
