b375c2ae14fcc80d4b9e95925bd38b46c36dc4d6
PavelBandal <pavel.bandal@ya.ru>
2017-10-01 16:14:59 +0300

    Конвертер

---
+++ 03. ifs/Конвертер/Конвертер/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите в сантиметрах");
+            int a = int.Parse (Console.ReadLine());
+            double b = (double)a / 100.0;
+            Console.WriteLine("в метрах это "+b);
+            Console.ReadLine();
+
+        }   
+    }
+}
