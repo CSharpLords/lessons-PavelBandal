2e73df3e094c5b4a784abdb85a3ba9a05b3cb2e1
PavelBandal <pavel.bandal@ya.ru>
2017-10-15 19:16:23 +0300

    signx

---
+++ 05. switches/signx/signx/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("число");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a > 0)
+            {
+                Console.WriteLine("1");
+                Console.ReadLine();
+            }
+            else if (a < 0)
+            {
+                Console.WriteLine("-1");
+                Console.ReadLine();
+            }
+            else if (a == 0)
+            {
+                Console.WriteLine("0");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
