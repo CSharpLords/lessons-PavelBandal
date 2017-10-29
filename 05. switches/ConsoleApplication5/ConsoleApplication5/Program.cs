e9756afaaebc4b8b8a8932943ea13ef1ec27ea52
PavelBandal <pavel.bandal@ya.ru>
2017-10-29 16:11:47 +0300

    0y80y8

---
+++ 05. switches/ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number please.");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number please.");
+            int b = int.Parse(Console.ReadLine());
+        }
+        static void SravniChisla(int a, int b)
+        {
+            if (a == 0 && b == 0)
+            {
+                Console.WriteLine("+");
+                Console.ReadLine();
+            }
+            else if (a < 0 && b < 0)
+            {
+                Console.WriteLine("+");
+                Console.ReadLine();
+            }
+            else if (a > 0 && b > 0)
+            {
+                Console.WriteLine("+");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("-");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+    
