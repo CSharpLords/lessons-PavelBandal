fe1a5f165627ffc4c19371d1f3719e0b50a05e1b
PavelBandal <32432734+PavelBandal@users.noreply.github.com>
2017-11-12 17:06:57 +0300

    078

--- 09. while/CheckPassword/CheckPassword/Program.cs
+++ 09. while/CheckPassword/CheckPassword/Program.cs
@@ -6,27 +6,35 @@ using System.Threading.Tasks;
 
 namespace CheckPassword {
 	class Program {
-		static void Main(string[] args) {
-			string password = "Супер пароль!";
-			Console.WriteLine("Чтобы пользоваться моей программой, введи пароль!");
-			string answer = Console.ReadLine();
-			
-			if (answer == password) {
-				Console.WriteLine("Ок, правильно. Жми Enter, чтобы пройти!");
-				Console.ReadLine();
-				Console.Clear();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string password = "пароль";
+                Console.WriteLine("Чтобы пользоваться моей программой, напиши пароль!");
+                string answer = Console.ReadLine();
 
-				StartProgram();
-			}
-			else {
-				Console.WriteLine("Неправильно! Хаха, ты не пройдешь!");
-				Console.ReadLine();
-				Environment.Exit(0);
-			}
-		}
+                if (answer == password)
+                {
+                    Console.WriteLine("Ок, правильно. Жми Enter, чтобы пройти!");
+                    Console.ReadLine();
+                    Console.Clear();
+
+                    StartProgram();
+                }
+                else
+                {
+
+                    Console.WriteLine("Чтобы пользоваться моей программой, напиши пароль!");
+                    string answe = Console.ReadLine();
+
+                }
+            }
+        }
 
 		static void StartProgram() {
 			Quest quest = new Quest();
 		}
 	}
 }
+
