8046e23558075ebe32753952c7cf49dd7372bd15
PavelBandal <32432734+PavelBandal@users.noreply.github.com>
2017-11-26 16:53:57 +0300

    222222222222222222222222222222222222222222222222222

---
+++ 10/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int attempt = 0;
+            int a = 0;
+            while (attempt < 6)
+            {
+                Console.WriteLine("Введите  число");
+                int b = int.Parse(Console.ReadLine());
+                a = a + b;
+                attempt = attempt + 1;
+                Console.WriteLine("Сумма этих чисел равна "+a);
+                
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
