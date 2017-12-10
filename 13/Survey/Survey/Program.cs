1f8f7468f7e1fd0fdcc7e6e35933e55c72dd5ad9
PavelBandal <32432734+PavelBandal@users.noreply.github.com>
2017-12-10 17:01:41 +0300

    Контр-тролльная работа

---
+++ 13/Survey/Survey/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("У тебя есть комп?");
+            string answer1 = Console.ReadLine();
+            if (answer1 == "нет")
+            {
+                Console.WriteLine("Ну, тогда твой IQ = 0, и нам неочем с тобой говорить.");
+                Console.ReadKey();
+            }
+            else if (answer1 == "да")
+            {
+                Console.WriteLine("Сколько часов в неделю ты сидишь в компе?");
+                int answer2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("А ты большую часть времени работаешь или отдыхаешь?");
+                string answer3 = Console.ReadLine();
+                if (answer2 < 14)
+                {
+                    if (answer3 == "работаю")
+                    {
+                        Console.WriteLine("Ты и отдохнуть не забывай");
+                        Console.ReadKey();
+                    }
+                    else if (answer3 == "отдыхаю")
+                    {
+                        Console.WriteLine("Хорошо, что ты знаешь меру");
+                        Console.ReadKey();
+                    }
+                }
+                else if (answer2 > 13)
+                {
+                    if (answer3 == "работаю")
+                    {
+                        Console.WriteLine("Пожалей себя!!");
+                        Console.ReadKey();
+                    }
+                    else if (answer3 == "отдыхаю")
+                    {
+                        Console.WriteLine("Не расстраивай свою маму и здоровье таким поведением!");
+                        Console.ReadKey();
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
---
+++ 13/Vova/Vova/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Какую вещь ты хочешь найти мой юный падаван?");
+            Console.WriteLine("Вот список: портфель, тапочки, XBox, телефон, записка с паролем от Dota2");
+            string answer = Console.ReadLine();
+            if (answer == "носки")
+            {
+                Console.WriteLine("Твои носки остались в школе внутри аптечки 5г");
+                Console.ReadKey();
+            }
+            else if (answer == "портфель")
+            {
+                Console.WriteLine("Он на твоей спине");
+                Console.ReadKey();
+            }
+            else if (answer == "тапочки")
+            {
+                Console.WriteLine("На ногах");
+                Console.ReadKey();
+            }
+            else if (answer == "XBox")
+            {
+                Console.WriteLine("Твой Xbox на люстре");
+                Console.ReadKey();
+            }
+            else if (answer == "телефон")
+            {
+                Console.WriteLine("В ванной");
+                Console.ReadKey();
+            }
+            else if (answer == "записка с паролем от Dota2")
+            {
+                Console.WriteLine("На моём экране");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Либо ты грамотей, либо я не знаю такой вещи");
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
