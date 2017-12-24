83786d740155962648b5f8dcab7a174eb774d5c6
PavelBandal <32432734+PavelBandal@users.noreply.github.com>
2017-12-24 16:35:07 +0300

    тыпрочёлэтооммойсэнсэй

--- 15. arrays/LinedPaper/Form1.Designer.cs
+++ 15. arrays/LinedPaper/Form1.Designer.cs
@@ -23,21 +23,36 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.SuspendLayout();
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.Color.Linen;
-			this.ClientSize = new System.Drawing.Size(284, 262);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(389, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ЖМАКАЙ МЕНЯ ПОЛНОСТЬЮ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(824, 575);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
 
 		}
 
 		#endregion
+
+        private System.Windows.Forms.Button button1;
 	}
 }
 
