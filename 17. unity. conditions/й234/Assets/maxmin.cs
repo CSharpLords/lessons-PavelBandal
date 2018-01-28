e0cb2a28dcf6ddd7f87aa56b8fe0b1fa92f7d5ab
PavelBandal <32432734+PavelBandal@users.noreply.github.com>
2018-01-28 17:21:56 +0300

    maxmin

---
+++ 17. unity. conditions/й234/Assets/maxmin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class maxmin : MonoBehaviour {
+	double y = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (y < transform.position.y){ 
+			y = transform.position.y;
+			print (y);
+		}
+
+
+		
+	}
+}
