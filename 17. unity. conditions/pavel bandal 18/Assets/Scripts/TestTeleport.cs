b8c76a0a1b30e0ccac1704e2204cdcac0d546e18
PavelBandal <32432734+PavelBandal@users.noreply.github.com>
2018-02-04 17:11:30 +0300

    Teleport

---
+++ 17. unity. conditions/pavel bandal 18/Assets/Scripts/TestTeleport.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTeleport : MonoBehaviour {
+	float time = 0; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time = time + Time.deltaTime;
+		if (time >= 15)
+		{
+			time = 0;
+			transform.position = new Vector3(0,0,-25);
+		}
+	}
+}
---
+++ 17. unity. conditions/pavel bandal 18/Assets/Scripts/Win.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour {
+	public Transform item;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dist = Vector3.Distance(item.position, transform.position);
+		if (dist < 3)
+		{
+			print ("You Win!");
+		}
+	}
+}
