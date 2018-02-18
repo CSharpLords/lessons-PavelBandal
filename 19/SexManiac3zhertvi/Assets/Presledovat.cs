ade9f3d84fb77984e15a2fdc15448192f35c31ca
PavelBandal <32432734+PavelBandal@users.noreply.github.com>
2018-02-18 18:46:58 +0300

    19

---
+++ 19/SexManiac3zhertvi/Assets/Presledovat.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Presledovat : MonoBehaviour {
+	public Transform[] objects;
+	// Use this for initialization
+	Vector3 objectPosition;
+
+	void Start () {
+		float min = 999;
+		for (int i = 0; i < objects.Length; i++) {
+			float dist = Vector3.Distance (transform.position, objects[i].position);
+			if (min > dist) {
+				min = dist;
+				objectPosition = objects[i].position;
+				print (objectPosition);
+
+			}
+
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction  = objectPosition - transform.position;
+		direction.Normalize();
+		direction = direction * 5;
+		transform.Translate(direction * Time.deltaTime);
+
+
+
+	}
+}
