0ccea1f37d38be5907cae83f12ca28514e655434
PavelBandal <32432734+PavelBandal@users.noreply.github.com>
2018-02-18 18:57:39 +0300

    ЙЦВК

---
+++ 19/Sexual Maniak (otkroi mena)/Assets/SexManiac.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SexManiac : MonoBehaviour {
+	 
+	public Transform target; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector3 direction  = target.position - transform.position;
+
+		direction.Normalize();
+
+		direction = direction * 5;
+
+		transform.Translate(direction * Time.deltaTime);
+
+	}
+}
