a6602a772e96de0d9d336fa3b6dc39d27d448e6a
PavelBandal <32432734+PavelBandal@users.noreply.github.com>
2018-03-25 14:58:02 +0300

    jhjp0

---
+++ 15. arrays/15 03 2018 Заяц/15 03 2018 Заяц/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя зайца ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Вашего зайца зовут " + name);
+            Console.WriteLine("Вот он:");
+            Console.WriteLine("(\\___/)"); 
+            Console.WriteLine("(='.'=)");
+            Console.WriteLine("(\")_(\")");
+            Console.ReadLine();
+            
+        }
+    }
+}
