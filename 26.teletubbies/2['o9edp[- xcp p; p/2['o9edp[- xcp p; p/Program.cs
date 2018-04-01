a185262a87325333f49a0afeb646a09dc90cc5f7
PavelBandal <32432734+PavelBandal@users.noreply.github.com>
2018-04-01 17:02:14 +0300

    4234234

---
+++ 26.teletubbies/2['o9edp[- xcp p; p/2['o9edp[- xcp p; p/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CalcPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Введите степень: ");
+            int e = int.Parse(Console.ReadLine());
+            int a = n;
+            for (int i = 0 ; i < e; i++)
+            {
+                a = n * n;
+            }
+            Console.WriteLine(a);
+            Console.Read();
+        }
+    }
+}
---
+++ 26.teletubbies/PairTubbies/PairTubbies/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PairSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckPairs("namespace ConsoleApplication1 {class Program {static void Main(string[] args) {}", "{", "}");
+            CheckPairs("graphics = e.Graphics;graphics.SmoothingMode = SmoothingMode.AntiAlias;graphics.Clear(Color.FromArgb(0x44444400));DrawCar(carX, 200);", "(", ")");
+            Console.ReadLine();
+        }
+        static void CheckPairs(string text, string symbolsOpen, string symbolsClose)
+        {
+            int SO = 0;
+            int SC = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+
+                if (text[i].ToString() == symbolsOpen)
+                {
+                    SO = SO + 1;
+                }
+                else if (text[i].ToString() == symbolsClose)
+                {
+                    SC = SC + 1;
+                }
+            }
+            if (SO == SC)
+            {
+                Console.WriteLine("ЭЬТА Щ4ИдеВирЬЬ");
+            }
+            else
+            {
+                Console.WriteLine("Сори, но ты фекалиекодер");
+            }
+        }
+    }
+}
+
+               
+
